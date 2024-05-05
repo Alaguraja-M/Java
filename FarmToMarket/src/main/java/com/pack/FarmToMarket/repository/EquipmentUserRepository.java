38e31f24a28c1d1c878bedb6ba558e10aea7285d
Alaguraja M <alagurajamr@gmail.com>
2024-05-05 22:49:46 +0530

    Error in Junit
    Removing the codes

--- FarmToMarket/src/main/java/com/pack/FarmToMarket/repository/EquipmentUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pack.FarmToMarket.repository;
- 
-import org.springframework.data.jpa.repository.JpaRepository;
- 
-import com.pack.FarmToMarket.entity.EquipmentUser;
- 
-public interface EquipmentUserRepository extends JpaRepository<EquipmentUser, Integer>{
- 
-}
